141d850e93e3a5b5e3f7ac9f23abbb3a265d9015
amir4976 <amirhosianahmadi4976@gmail.com>
2023-05-08 23:42:22 +0430

    next

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -3,57 +3,62 @@ import { useRouter } from 'next/router'
 import Layout from '../layout/Layout'
 import { NEXT_URL } from '@/config'
 
-function eventPage({events}) {
-    const router = useRouter()
+function eventPage({ events }) {
+  const router = useRouter()
   const { id } = router.query
   console.log(events)
   console.log(NEXT_URL)
   return (
     <>
-    <Layout title={'event'}>
-      <h1>{events.name}</h1>
- 
-    </Layout>
+      <Layout title={'event'}>
+        <h1>{events.name}</h1>
+
+      </Layout>
     </>
   )
 }
 
 
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   const res = await fetch(`${NEXT_URL}/api/events`)
   const events = await res.json()
 
-  const paths = events.map(evt=>({
-    params:{slug: evt.slug }
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug }
   }))
-  return{
-      paths,
-      fallback: false ,
+
+
+  return {
+    paths,
+    fallback: true
   }
 }
 
 
 
 
-export async function getStaticProps ({query:slug}){
-  
- const res = await fetch(`${NEXT_URL}/api/events/${slug.id}`)
-  const events= await res.json()
-  
-  return{
-    props:{events:events[0]}
+export async function getStaticProps({ params: { slug } }) {
+
+  const res = await fetch(`${NEXT_URL}/api/events/${slug.id}`)
+  const events = await res.json()
+
+  return {
+    props: {
+       events: events[0] ,
+      },
+      revalidate: 1
   }
-} 
+}
 
 
 // get server side props
 
 // export async function getServerSideProps ({query:slug}){
-  
+
 //  const res = await fetch(`${NEXT_URL}/api/events/${slug.id}`)
 //   const events= await res.json()
-  
+
 //   return{
 //     props:{events:events[0]}
 //   }
