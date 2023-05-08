d03e4c84cb28de2d5110beebd8de0acb5034bf97
amir4976 <amirhosianahmadi4976@gmail.com>
2023-05-08 20:43:21 +0430

    next

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-
+import Layout from '../layout/Layout'
 
 function eventPage() {
     const router = useRouter()
   const { id } = router.query
 
   return (
-    <div>eventPage {id}</div>
+    <>
+    <Layout title={'event'}>
+      <h1>event select</h1>
+
+    </Layout>
+    </>
   )
 }
 
